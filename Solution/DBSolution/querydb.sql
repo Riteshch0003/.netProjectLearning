18e515d6236bb1abc4bb7f0c175f2695f33e5b3f
AjinkyaBTambade <ajinkyatambade143@gmail.com>
2024-11-17 20:44:12 +0530

    database updated

--- Solution/DBSolution/querydb.sql
+++ Solution/DBSolution/querydb.sql
@@ -3,3 +3,13 @@ SELECT * FROM Users;
 SELECT * FROM Posts;
 
 SELECT * FROM Comments;
+
+
+INSERT INTO Posts (Title, Content, UserId) VALUES
+('Travel Diaries', 'My experience exploring the Himalayan trails..', 2);
+
+
+
+INSERT INTO Comments (PostId, UserId, Content) VALUES
+    (2, 2,'Loved this! Especially the part about market research.');
+
